9f6d3a462962287d61179a6356071c48b735d903
Scott Daugherty <srdaugherty@gmail.com>
2017-08-25 10:18:20 -0400

    New CSV load approach
    Initial commit to branch

--- data/louisvillekygov-to-awscivicdata.py
+++ data/louisvillekygov-to-awscivicdata.py
@@ -13,10 +13,15 @@ import collections
 import warnings 
 import pymysql	
 import pymysql.cursors
+import redis
 from pprint import pprint
+from hashlib import blake2b
+from datetime import datetime
+from datetime import timedelta
 pymysql.install_as_MySQLdb()
 	
-
+INSERT_SQL = ""
+DELETE_SQL = ""
 
 # CSV to MySQL Utilities credited to --> https://bitbucket.org/richardpenman/csv2mysql
 # suppress annoying mysql warnings
@@ -130,7 +135,11 @@ def get_insert(table, header):
 	field_names = ', '.join(header)
 	field_markers = ', '.join('%s' for col in header)
 	return 'INSERT INTO %s (%s) VALUES (%s);' % \
-		(table, field_names, field_markers)
+		(table, field_names + ',insert_timestamp', field_markers + ', %s')
+
+def get_delete(table):
+	return 'DELETE FROM %s WHERE incident_number = %s;' % \
+		(table, '%s')
 
 
 def format_header(row):
@@ -206,6 +215,133 @@ def main(input_file, user, password, host, table, database, max_inserts=10000):
 	db.commit()
 	print('Done!')
 
+def parse_csv(input_file, user, password, host, table, database, max_inserts=10000):
+	print("Importing `%s' into MySQL database `%s.%s'" % (input_file, database, table))
+	db = pymysql.connect(host=host, user=user, passwd=password, charset='utf8')
+	redisCn = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+	
+	cursor = db.cursor()
+	
+	db.select_db(database)
+
+	# define table
+	#print('Analyzing column types ...')
+	#col_types = get_col_types(input_file)
+	#print(col_types)
+
+	header = None
+
+	print("url: '{}'".format(input_file))
+
+	context = ssl._create_unverified_context()
+
+	with open("/Users/scottdaugherty/Documents/Alexa/Louisville/carnegie/data/Crime_Data_2017_0_b.csv") as response:
+	#with urllib.request.urlopen(input_file, context=context) as response:
+		#csv_str = io.StringIO(response.read().decode('utf-8'))
+		
+		reader = csv.reader(response, dialect='excel', delimiter=',', quotechar='"', skipinitialspace=True)
+		
+		# process header, generate SQL
+		header = format_header(next(reader, None))
+		global INSERT_SQL
+		INSERT_SQL = get_insert(table, header)
+		global DELETE_SQL
+		DELETE_SQL = get_delete(table)
+
+		# this appears to sort from the second row of the file (which is ideal) since next() was called above
+		# TODO test the above assumption
+		reader = sorted(reader, key=lambda row: row[0], reverse=False)
+
+		incidents = []
+
+		for i, row in enumerate(reader):
+			if i == 0 or row[0] == incidents[0][0]:
+				# incident # of this row matches that of the last row
+				incidents.append(row)
+			else:
+				# new incident #; process the previous incident rows
+				processIncident(incidents, cursor, redisCn)
+
+				# reset
+				incidents = []
+				incidents.append(row)
+				
+				if i % 100 == 0:
+					print('Committing rows to database ...')
+					db.commit()
+
+		# final entry
+		processIncident(incidents, cursor, redisCn)
+
+	# commit rows to database
+	print('Committing rows to database ...')
+	db.commit()
+	print('Done!')
+
+def processIncident(incidents, cursor, redisCn):
+	incidentText = ""
+	# concat the column data from every row pertaining to this incident (minus the ID column as it changes with every new file)
+	for incident in incidents:
+		incidentText += ",".join(incident[0:-1])
+
+	incidentNum = incidents[0][0]
+	hashText = hash_it(incidentText)
+
+	incidentExists, incidentAltered = checkIncident(incidentNum, hashText, redisCn)
+
+	if incidentExists:
+		if incidentAltered:
+			deleteIncident(incidentNum, cursor, redisCn)
+			insertIncident(incidents, hashText, cursor, redisCn)
+	else:
+		insertIncident(incidents, hashText, cursor, redisCn)
+
+
+def checkIncident(incidentNum, hashText, redisCn):
+	# three possible scenarios:
+	# 1) inc isn't present in redis; return False, False (results in insert to mysql and redis)
+	# 2) inc is present in redis and hash matches; return True, False (results in no changes in database or cache)
+	# 3) inc is present in redis but hash does not match; return True, True (delete from mysql and redis, insert as in step 1)
+	incidentExists = False
+	incidentAltered = False
+
+	hashLookup = redisCn.get(incidentNum)
+
+	if hashLookup != None:
+		hashLookup = hashLookup.decode("utf-8")
+		incidentExists = True
+
+		if(hashLookup != hashText):
+			incidentAltered = True
+
+	return incidentExists, incidentAltered
+
+def insertIncident(incidents, hashText, cursor, redisCn):
+	# load into MYSQL
+	for incident in incidents:
+		# TODO why is this padding necessary?
+		while len(incident) < 15: #len(header):
+			incident.append('') # this row is missing columns so pad blank values
+
+		incident.append(datetime.now())
+
+		cursor.execute(INSERT_SQL, incident)
+
+	# set Redis key/value	
+	redisCn.set(incidents[0][0], hashText)
+	
+	print("Incident inserted: " + incidents[0][0])
+
+
+def deleteIncident(incidentNum, cursor, redisCn):
+	# delete from MYSQL
+	cursor.execute(DELETE_SQL, incidentNum)
+	# delete Redis key
+	redisCn.delete(incidentNum)
+
+	print("Incident deleted:  " + incidentNum)
+
+
 def get_crime_datasets():
 	
 	context = ssl._create_unverified_context()
@@ -224,29 +360,34 @@ def get_crime_datasets():
 	
 	return distributions
 
+def hash_it(input):
+	h = blake2b(digest_size=20)
+	h.update(bytearray(input, 'utf-8'))
+	return h.hexdigest()
+
 def get_csv_file_name():
 	context = ssl._create_unverified_context()
 	with urllib.request.urlopen("https://data.louisvilleky.gov/api/3/action/package_show?id=crime-data", context=context) as data_file:
 		data = json.load(data_file)
 
 		file_name = data["result"][0]["resources"][0]["url"]
-		print(file_name)
 		return file_name
 
 def lambda_handler(event, context):
-	#print("Received event: " + json.dumps(event, indent=2))
-	print("beginning handler")
+	startTime = datetime.now()
 
-	#main(args.input_file, args.user, args.password, args.host, args.table, args.database)
-	# example file: https://data.louisvilleky.gov/sites/default/files/Crime_Data_2017_2.csv
-	print('executing main... ')
+	print("Execution for %s" % startTime)
 
 	crimefile = get_csv_file_name()
 	if("2017" in crimefile):
 		print(crimefile)
-		main(crimefile, 'rw', 'civicdataalliance', 'civicdata.crogewynsqom.us-east-1.rds.amazonaws.com', 'crimeData', 'louisvilleky', max_inserts=10)
+		#main(crimefile, 'rw', 'civicdataalliance', 'civicdata.crogewynsqom.us-east-1.rds.amazonaws.com', 'crimeDataQA', 'louisvilleky', max_inserts=10)
+		parse_csv(crimefile, 'rw', 'civicdataalliance', 'civicdata.crogewynsqom.us-east-1.rds.amazonaws.com', 'crimeDataQA', 'louisvilleky', max_inserts=10)
+
+	diffMinutes = (datetime.now() - startTime) / timedelta(minutes=1)
+	print("Execution time: %dm:%ds" % (diffMinutes, diffMinutes * 60))
 
 	return "handler completed"
     
-#lambda_handler(None, None)
+lambda_handler(None, None)
 
